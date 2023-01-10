bfff909a7fe227c3251ecf78a4a641432cd865a6
Artem Demeniev <myfirstmillio93@gmail.com>
2023-01-10 10:54:50 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+        let a = str.split('').reverse().join('');
+        let num = parseInt(a);
+        return Math.abs(num);
 }
+
